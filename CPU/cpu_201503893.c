718cffa94bf119843b636adf7cf9e523ccc409ca
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2020-12-16 01:04:49 -0600

    Update modulo cpu

--- CPU/cpu_201503893.c
+++ CPU/cpu_201503893.c
@@ -22,13 +22,100 @@ struct sysinfo inf;
 int contador = 0;
 long total_memoria = 0;
 
+
+void imprimir_estado(struct task_struct *task_next, struct seq_file * m)
+{
+    if(task_next->state == -1){
+        seq_printf(m,"      \"Estado\":\"NO EJECUTABLE\"");
+    }else if(task_next->state == 0){
+        seq_printf(m,"      \"Estado\":\"EJECUCION\"");
+    }else if(task_next->state == 1){
+        seq_printf(m,"      \"Estado\":\"SUSPENDIDO\""); //INTERRUPTIBLE
+    }else if(task_next->state == 2){
+        seq_printf(m,"      \"Estado\":\"SUSPENDIDO\""); //UNINTERRUPTIBLE
+    }else if(task_next->state == 4){ 
+        seq_printf(m,"      \"Estado\":\"DETENIDO\""); //STOPEED
+    }else if(task_next->state == 8){ 
+        seq_printf(m,"      \"Estado\":\"DETENIDO\"");//TRACED
+    }else if(task_next->state == 16){
+        seq_printf(m,"      \"Estado\":\"ZOMBIE\"");
+    }else{
+        seq_printf(m,"      \"Estado\":\"DESCONOCIDO\"");
+    }
+}
+
+
+void recorrer_tareas(struct task_struct *task, struct seq_file * m, int num)
+{
+  struct task_struct *task_next;
+  struct list_head *list;
+  
+  int tmp = contador;
+  contador = 1;
+  
+  int cant = 0;
+  list_for_each(list, &task->children) {
+    task_next = list_entry(list, struct task_struct, sibling);
+    if(task_next == 0){
+        continue;
+    }
+    cant = cant+1;
+  }
+  
+  list_for_each(list, &task->children) {
+    task_next = list_entry(list, struct task_struct, sibling);
+
+    if(task_next == 0){
+        continue;
+    }
+    seq_printf(m,"      {\n");    
+    seq_printf(m,"      \"PID\":%d,\n",task_next->pid);	
+    seq_printf(m,"      \"Nombre\":\"%s\",\n",task_next->comm);
+    seq_printf(m,"      \"Usuario\":\"%d\",\n",task_next->cred->uid);
+    
+    //IMPRIMIMOS EL ESTADO
+    imprimir_estado(task_next,m);
+
+    seq_printf(m,",\n");
+
+    //============= Porcentaje RAM ============
+    long memoria_utilizada = 0;
+    if(task_next->mm){
+        memoria_utilizada = (task_next->mm->total_vm <<(PAGE_SHIFT -10));
+        memoria_utilizada = (memoria_utilizada/58603);
+    }
+
+    //memoria_utilizada*100/total_memoria
+    seq_printf(m,"      \"PorcentajeRam\": %i",memoria_utilizada);
+    seq_printf(m,",\n");
+
+    //============= PPID ======================
+    seq_printf(m,"      \"PPID\":%d\n",num);
+    
+    if(cant > 1){
+        seq_printf(m,"      },\n");
+        cant = cant - 1;
+    }else {
+        if(num == 2){
+            seq_printf(m,"      }\n");    
+        }else{
+            seq_printf(m,"      },\n");
+        }
+    }
+    //EN ESTA PARTE VOLVEMOS A LLAMAR A LA FUNCION PARA RECORRER A LOS HIJOS
+    recorrer_tareas(task_next, m, task_next->pid); 
+  }
+  contador = tmp;
+}
+
+
 static int write_file(struct seq_file * m, void *v) {	
-    cont = 1;
+    contador = 1;
     si_meminfo(&inf);
     total_memoria 	= (inf.totalram * 4)/1024;    
     seq_printf(m,"{\n");
     seq_printf(m,"      \"struct_lista_procesos\":[\n");
-    dfs(&init_task, m, 0);
+    recorrer_tareas(&init_task, m, 0);
     seq_printf(m,"      ]\n");
     seq_printf(m,"}\n");
     return 0;
