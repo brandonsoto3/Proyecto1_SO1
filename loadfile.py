b4e03fe14c1c3f7d48ca5b05836e75d6a4a414ec
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2020-12-18 03:35:06 -0600

    Update loadfile.py

--- loadfile.py
+++ loadfile.py
@@ -1,8 +1,12 @@
-from locust import User, task, between
+from locust import HttpUser, task, between
 
-class MyUser(User):
-    @task
-    def my_task(self):
-        print("executing my_task")
+class MyUser(HttpUser):
+    wait_time = between(5, 15)
 
-    wait_time = between(0.5, 10)
+    @task(4)
+    def index(self):
+        self.client.get("http://3.138.204.175/procesos")
+
+    @task(1)
+    def about(self):
+        self.client.get("http://3.138.204.175/ram")
